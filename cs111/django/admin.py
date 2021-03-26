5ed4278ed635ec9eb782481a6b44eb6480c85bc9
Jon Eyolfson <jon@eyl.io>
2021-03-26 15:27:12 -0400

    Added Lab and Role models

--- cs111/django/admin.py
+++ cs111/django/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import File, Lecture
+from .models import File, Lecture, Lab, Role
 
 admin.site.register(File)
 admin.site.register(Lecture)
+admin.site.register(Lab)
+admin.site.register(Role)
--- cs111/django/models.py
+++ cs111/django/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 from .storages import OverwriteFileSystemStorage
 
 class File(models.Model):
@@ -16,3 +18,33 @@ class Lecture(models.Model):
 
     def __str__(self):
         return f'{self.title} (Lecture {self.number})'
+
+class Lab(models.Model):
+    number = models.SmallIntegerField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    version = models.CharField(max_length=255)
+    pdf = models.FileField(upload_to='cs111/labs', storage=OverwriteFileSystemStorage)
+
+    def __str__(self):
+        return f'{self.title} (Lab {self.number})'
+
+class Role(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='role',
+    )
+    INSTRUCTOR = 'I'
+    TA = 'T'
+    STUDENT = 'S'
+    ROLE_CHOICES = [
+        (INSTRUCTOR, 'Instructor'),
+        (TA, 'TA'),
+        (STUDENT, 'Student'),
+    ]
+    role = models.CharField(
+        max_length=1, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        display = self.get_role_display()
+        return f'{self.user} ({display})'
