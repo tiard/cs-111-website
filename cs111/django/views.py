47c7e2f126f867be400e480268d37088a869dba5
Jon Eyolfson <jon@eyl.io>
2021-04-01 20:44:21 -0400

    Revert "Added solutions"
    This reverts commit e9c0bc94f6f53d7e31fce606cfa987a9663a7863.

--- cs111/django/views.py
+++ cs111/django/views.py
@@ -18,13 +18,7 @@ class LabsView(generic.ListView):
         return Lab.objects.order_by('number')
 
 def index(request):
-    try:
-        solutions = File.objects.get(file='cs111/solutions.pdf')
-    except File.DoesNotExist:
-        solutions = None
-    return render(request, 'cs111/index.html', {
-        'solutions': solutions,
-    })
+    return render(request, 'cs111/index.html', {})
 
 def labs(request):
     return render(request, 'cs111/labs.html', {})
