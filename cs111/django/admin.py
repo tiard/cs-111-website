ee9c7e0b491d5cb1047df8cfc5142e7e414d99ce
Jon Eyolfson <jon@eyl.io>
2021-06-11 12:43:28 -0400

    Added final exam grade to admin interface

--- cs111/django/admin.py
+++ cs111/django/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import File, Lecture, Lab, Role, LabGrade, MidtermGrade
+from .models import File, Lecture, Lab, Role, LabGrade, MidtermGrade, FinalExamGrade
 
 admin.site.register(File)
 admin.site.register(Lecture)
@@ -8,3 +8,4 @@ admin.site.register(Lab)
 admin.site.register(Role)
 admin.site.register(LabGrade)
 admin.site.register(MidtermGrade)
+admin.site.register(FinalExamGrade)
