695b1940db9a28cec9c0b073db6c5bc239b9ec19
Jon Eyolfson <jon@eyl.io>
2021-09-20 14:56:44 -0700

    Improve addstudents

--- cs111/django/management/commands/addstudents.py
+++ cs111/django/management/commands/addstudents.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from cs111.django.models import Role
 
-from ._private import add_user
+from ._private import add_user, update_user
 
 import argparse
 import csv
@@ -21,8 +21,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         reader = csv.reader(options['csv'])
-        is_body = False
+
+        # Get all the existing usernames
         usernames = set()
+        for role in Role.objects.all():
+            usernames.add(role.user.username)
+
+        is_body = False
         for row in reader:
             if 'Name' in row:
                 is_body = True
@@ -52,10 +57,25 @@ class Command(BaseCommand):
 
             ucla_id = int(row[0].replace('-', ''))
 
+            # Check for any repeat students
+            if username in usernames:
+               try:
+                   updated = update_user(username, ucla_id)
+               except:
+                   self.stdout.write(self.style.ERROR(f'Failed to update student "{username}"'))
+                   raise
+
+               if updated:
+                   continue
+               else:
+                   assert not Role.objects.filter(ucla_id=ucla_id).exists()
+                   self.stdout.write(self.style.NOTICE(f'Conflict with student "{username}" ({ulca_id})'))
+
             assert username not in usernames
             usernames.add(username)
 
-            if add_user(username, email, first_name, last_name, Role.STUDENT, ucla_id=ucla_id):
-                self.stdout.write(self.style.SUCCESS(f'Added student "{username}"'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Student exists "{username}"'))
+            try:
+                add_user(username, email, first_name, last_name, Role.STUDENT, ucla_id=ucla_id)
+            except:
+                self.stdout.write(self.style.ERROR(f'Failed to add student "{username}"'))
+                raise
