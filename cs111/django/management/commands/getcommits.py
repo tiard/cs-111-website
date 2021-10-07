1cf3d780683bab18bccfc228cd8698d8d08dff99
Jon Eyolfson <jon@eyl.io>
2021-10-07 14:40:24 -0700

    New lab naming

--- cs111/django/management/commands/getcommits.py
+++ cs111/django/management/commands/getcommits.py
@@ -58,7 +58,7 @@ class Command(BaseCommand):
                     for patch in git_repo.diff(commit.parents[0], commit):
                         delta = patch.delta
                         if delta.status == pygit2.GIT_DELTA_ADDED or delta.status == pygit2.GIT_DELTA_MODIFIED:
-                            if delta.new_file.path.startswith(f'lab-{lab.number:02}'):
+                            if delta.new_file.path.startswith(f'lab{lab.number}'):
                                 found = True
                                 break
                     if found:
